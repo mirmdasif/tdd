289ca914802d4c3227acff03e8f2421466375b22
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-03-01 11:59:09 +0600

    Pass first test by hardcoding

---
+++ src/main/java/net/asifhossain/tdte/Template.java
@@ -0,0 +1,20 @@
+package net.asifhossain.tdte;
+
+/**
+ * @author asif.hossain
+ * @since 3/1/17.
+ */
+public class Template {
+
+    public Template(String template) {
+
+    }
+
+    public void set(String variable, String value) {
+
+    }
+
+    public String evaluate() {
+        return "Hello, Reader";
+    }
+}
