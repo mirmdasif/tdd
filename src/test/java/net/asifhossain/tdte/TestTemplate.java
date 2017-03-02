659d8a0df70117c55e6877090d5e3ef28d8897be
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-03-02 10:40:03 +0600

    Refactor test template optimized test methods

--- src/test/java/net/asifhossain/tdte/TestTemplate.java
+++ src/test/java/net/asifhossain/tdte/TestTemplate.java
@@ -1,5 +1,6 @@
 package net.asifhossain.tdte;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 /**
@@ -7,45 +8,28 @@ import static org.junit.Assert.*;
  * @since 3/1/17.
  */
 public class TestTemplate {
-    @Test
-    public void oneVariable() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Reader");
-        assertEquals("Hello, Reader", template.evaluate());
-    }
 
-    @Test
-    public void differentVariable() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Bob");
-        assertEquals("Hello, Bob", template.evaluate());
-    }
-
-    @Test
-    public void differentTemplate() throws Exception {
-        Template template = new Template("Hi, ${name}");
-        template.set("name", "Bob");
-
-        assertEquals("Hi, Bob", template.evaluate());
+    Template template;
+    @Before
+    public void setUp() {
+        template = new Template("${one}, ${two}, ${three}");
+        template.set("one", "1");
+        template.set("two", "2");
+        template.set("three", "3");
     }
 
     @Test
     public void multipleVariables() throws Exception {
-        Template template = new Template("${one}, ${two}, ${three}");
-
-        template.set("one", "1");
-        template.set("two", "2");
-        template.set("three", "3");
-
-        assertEquals("1, 2, 3", template.evaluate());
+        assertTemplatesEvaluateTo("1, 2, 3");
     }
 
     @Test
     public void unknownVariableAreIgnored() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Bob");
         template.set("unknown", "value unknown");
+        assertTemplatesEvaluateTo("1, 2, 3");
+    }
 
-        assertEquals("Hello, Bob", template.evaluate());
+    private void assertTemplatesEvaluateTo(String expected) {
+        assertEquals(expected, template.evaluate());
     }
 }
