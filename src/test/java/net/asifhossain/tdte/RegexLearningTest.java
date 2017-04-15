a77c385f7c2e44d91b0e79c44ead53cb4454ac35
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-04-15 16:31:05 +0600

    Learning spike for regex

---
+++ src/test/java/net/asifhossain/tdte/RegexLearningTest.java
@@ -0,0 +1,42 @@
+package net.asifhossain.tdte;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * @author asif.hossain
+ * @since 3/29/17.
+ */
+public class RegexLearningTest {
+    @Test
+    public void testHowGroupCountWorks() {
+        String haystack = "The needle shop sells needles";
+        String regex = "(needle)";
+
+        Matcher matcher = Pattern.compile(regex).matcher(haystack);
+
+        assertEquals(1, matcher.groupCount());
+    }
+
+    @Test
+    public void testFindStartAndEnd() {
+        String haystack = "The needle shop sells needles";
+        String regex = "(needle)";
+        Matcher matcher = Pattern.compile(regex).matcher(haystack);
+
+        assertTrue(matcher.find());
+        assertEquals("Wrong start index of first match", 4, matcher.start());
+        assertEquals("Wrong end index of first match", 10, matcher.end());
+
+        assertTrue(matcher.find());
+        assertEquals("Wrong start index of 2nd match", 22, matcher.start());
+        assertEquals("Wrong end index of 2nd match", 28, matcher.end());
+        assertFalse("Should not have any more matches", matcher.find());
+    }
+}
