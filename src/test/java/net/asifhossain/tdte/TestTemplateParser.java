dc1e97f5274de2e03a4da04bcb8d9c1720005f4b
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-04-16 15:56:08 +0600

    Refactored TestTemplateParser

--- src/test/java/net/asifhossain/tdte/TestTemplateParser.java
+++ src/test/java/net/asifhossain/tdte/TestTemplateParser.java
@@ -1,35 +1,38 @@
 package net.asifhossain.tdte;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertFalse;
-
 /**
  * @author asif.hossain
  * @since 4/16/17.
  */
 public class TestTemplateParser {
 
+    private TemplateParser parser;
+
+    @Before
+    public void initialize() {
+        parser = new TemplateParser();
+    }
+
     @Test
     public void testEmptyTemplatesRendersEmptyString() {
-        TemplateParser parser = new TemplateParser();
-        List<String> segments = parser.parse("");
-
-        assertEquals("Number of segments", 1, segments.size());
-        assertEquals("", segments.get(0));
+        assertSegments(parser.parse(""), "");
     }
 
     @Test
     public void testTemplateWithOnlyPlainText() {
         String template = "Text with no variable";
+        assertSegments(parser.parse(template), template);
+    }
 
-        TemplateParser parser = new TemplateParser();
-        List<String> segments = parser.parse(template);
-
-        assertEquals("Number of segments", 1, segments.size());
-        assertEquals(template, segments.get(0));
+    private void assertSegments(List<String> expected, String... actual) {
+        assertEquals("Number of segments", expected.size(), actual.length);
+        assertEquals(expected, Arrays.asList(actual));
     }
 }
